ac1b3ff2e7da32401c5c59b5c95c010839c318f6
Phil99fp <philipcotter42@gmail.com>
2022-03-09 18:06:45 +0000

    add favicon

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,44 +1,31 @@
-import React from "react"
+import React from "react";
 
-
-
-const Repo = ({user, results}) => {
-
+const Repo = ({ user, results }) => {
   // const resultsCard = ({eachRepos}) => {
   //   const {id, name} = eachRepos
   //    return <h1> {eachRepo.name}</h1>
   // }
 
   const displayResults = results.map((eachRepo) => {
-    
-  return (
-    <div key={eachRepo.name}>
-      <h1> Name: {eachRepo.name}</h1>
-      <h2> Stargazers: {eachRepo.stargazers_count}</h2>
-      <h2> Forks: {eachRepo.forks_count}</h2>
-      <h2> Issues: {eachRepo.open_issues}</h2>
-    </div>
-  );
-  
-})
-
+    return (
+      <div key={eachRepo.name}>
+        <h2> Name: {eachRepo.name}</h2>
+        <h2> Stargazers: {eachRepo.stargazers_count}</h2>
+        <h2> Forks: {eachRepo.forks_count}</h2>
+        <h2> Issues: {eachRepo.open_issues}</h2>
+      </div>
+    );
+  });
 
   return (
     <>
- 
-      
-        <div>
-          <h1> hello world</h1>
-          <div >
-            <ul>{displayResults}</ul>
-          </div>
-        </div>
-      
+      <div>
+        <ul>{displayResults}</ul>
+      </div>
     </>
   );
-}
+};
 
-export default Repo
-
+export default Repo;
 
 // name, forks, stargazers, issue count
